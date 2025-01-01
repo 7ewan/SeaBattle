6316be96a152fb97d799c54414edbed82f4b4cb6
7ewan <luckyewan380@gmail.com>
2025-01-01 23:37:37 +0300

    add quit from window game

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ import sqlite3
 pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
+
+running = True
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill('white')
+    pygame.display.flip()
+    clock.tick(30)
+pygame.quit()
